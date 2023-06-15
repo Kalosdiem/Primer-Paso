4d0ea8487445235354f89287852f8db67b0c3f54
Kalosdiem <djp1975nn@gmail.com>
2023-06-15 20:19:29 -0300

    Initial commit

---
+++ Ejercicio 4.js
@@ -0,0 +1,4 @@
+function invertir (string) {
+    return split(string)&&object4,object3,object2,object1&&join(object4,object3,object2,object1)
+}
+invertir(hola)
---
+++ formulario.js
@@ -0,0 +1,13 @@
+function validateEmail(email) {
+    const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+    return regex.test(email)
+  }
+  function validateForm() {
+    const emailInput = document.getElementById('email');
+    const email = emailInput.value;
+    if (!validateEmail(email)) {
+      alert('Por favor ingrese un correo electrónico válido.');
+    } else {
+      alert('Correo electrónico enviado correctamente.');
+    }
+  }
